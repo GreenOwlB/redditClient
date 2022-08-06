eec4cec2e791e74b943b04ca75b52ae1a970181f
GreenOwlB <88214758+GreenOwlB@users.noreply.github.com>
2022-08-06 22:58:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+import Header from './modules/Header';
+import Footer from './modules/Footer';
+import Home from './pages/Home';
+import Subreddit from './pages/Subreddit';
+import Post from './pages/Post';
+
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+        <Routes>
+          <Route path = "/" element={<Home />}/>
+          <Route path = "/:subreddit" element={<Subreddit />}/>
+          <Route path = "/:subreddit/:post" element={<Post />}/>
+        </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/pages/Subreddit.jsx
@@ -0,0 +1,9 @@
+const Subreddit = () => {
+    return (
+        <div>
+            <h2>Subreddit</h2>
+        </div>
+    )
+}
+
+export default Subreddit
